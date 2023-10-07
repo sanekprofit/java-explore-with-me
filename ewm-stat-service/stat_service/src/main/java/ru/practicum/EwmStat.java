d6376d2f0b6ef1643f951af542ae271edcb018ff
sanekprofit <zaev-alex2006@yandex.ru>
2023-10-08 00:32:06 +0300

    refactor: refactor docker-compose.yml

--- ewm-stat-service/stat_service/src/main/java/ru/practicum/EwmStat.java
+++ ewm-stat-service/stat_service/src/main/java/ru/practicum/EwmStat.java
@@ -4,8 +4,8 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class Main {
+public class EwmStat {
     public static void main(String[] args) {
-        SpringApplication.run(Main.class, args);
+        SpringApplication.run(EwmStat.class, args);
     }
 }
